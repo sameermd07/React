39587c08463d58afecbe2c7fca5b6977a0ea0581
sameermd07 <sameermd1857978@gmail.com>
2026-02-12 16:41:43 +0530

    Refactor App component to include Button and Student components; add styles for button and student elements.

--- hello-ts/src/Components/Todo/index.tsx
+++ hello-ts/src/Components/Todo/index.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 import TodoItem from "./todoitem/index";
-import "./style.css";
-
-interface ITodoItem {
-  name: string;
-  age: number;
+import "./style.css"
+interface ITodoItem{
+    name:string,
+    age:number
 }
 
-interface TodoProps {
-  items: ITodoItem[];
+interface TodoProps{
+    items:ITodoItem[]
 }
-
-const Todo: React.FC<TodoProps> = (props) => {
-  return (
-    <div className="todo-container">
-      <ol>
-        {props.items.map((item, index) => (
-          <TodoItem key={index} title={item.name} />
-        ))}
-      </ol>
-    </div>
-  );
-};
+const Todo:React.FC<TodoProps>=(props)=>{
+    return(
+        <div className="todo-container">
+            <ol>
+                {items.map((item)=>(
+               <TodoItem title={item.name}/>
+            ))}
+            </ol>
+        </div>
+    )
+}
 
 export default Todo;
